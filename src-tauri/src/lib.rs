3b9cd14a3b6c10ac1250cf6e836a3c7b5d83659d
Muhammad Zakarya <admin@devozilla.com>
2026-03-27 11:45:59 +0200

    test update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,7 +9,52 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_printer_v2::init())
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(
+            tauri_plugin_updater::Builder::new()
+                .header(
+                    "Authorization",
+                    format!("token {}", "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"),
+                )
+                .expect("failed to add authorization header")
+                .build(),
+        )
+        .setup(|app| {
+            use tauri::menu::{Menu, MenuItem};
+            use tauri::tray::{TrayIconBuilder, TrayIconEvent};
+
+            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let settings_i = MenuItem::with_id(app, "settings", "API Settings", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&settings_i, &quit_i])?;
+
+            let _tray = TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .on_menu_event(|app: &tauri::AppHandle, event| match event.id.as_ref() {
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    "settings" => {
+                        let _ = tauri::WebviewWindowBuilder::new(
+                            app,
+                            "api_settings",
+                            tauri::WebviewUrl::App("/api-settings".into()),
+                        )
+                        .title("API Configuration")
+                        .inner_size(400.0, 300.0)
+                        .resizable(false)
+                        .always_on_top(true)
+                        .build();
+                    }
+                    _ => {}
+                })
+                .build(app)?;
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
